56921b96200c66dc2ea5834e64a400ba1541e5c1
ChingizbekXurramov <chingizbekxurramov@gmail.com>
2024-05-07 00:45:47 -0700

    OOP-12

---
+++ OOP12/OOP12/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OOP12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Workers worker = new Workers("Oleg");
+
+            Console.Write("Vvedite vozrast ==> ");
+            worker.Age = int.Parse(Console.ReadLine());
+
+            Console.Write("Vvvedite staj ==> ");
+            worker.Experience = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Rabotnek  {worker.Name} imeet {worker.Age} let i staj {worker.Experience} let.");
+        }
+    }
+}
---
+++ OOP12/OOP12/Workers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP12
+{
+    internal class Workers
+    {
+        private string name;
+        private int age;
+        private int experience;
+
+        public Workers(string name)
+        {
+            this.name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value >=15 && value <= 100)
+                {
+                    age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error!!!!!!!!!!.    Vvedite vozrast ot 15 do 100.");
+                }
+            }
+        }
+
+        public int Experience
+        {
+            get { return experience; }
+            set
+            {
+                if (value >= 0)
+                {
+                    experience = value;
+                }
+                else
+                {
+                    Console.WriteLine("Error!!!!!!!!!. vvedite polojiteknoy znachiniya .");
+                }
+            }
+        }
+    }
+}
